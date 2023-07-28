10f811fb4097b0353ae95fe49e0cb220e5b06642
florian <florian.batt@hotmail.fr>
2023-07-28 12:07:35 +0200

    Done

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
   templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  styleUrls: ['./exercice1-enfant.component.css'],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() public result: number;
 
-  ngOnInit() {
+  @Output() public resultEvent: EventEmitter<number> = new EventEmitter();
+
+  public more() {
+    this.resultEvent.emit(this.result + 1);
   }
 
+  public less() {
+    this.resultEvent.emit(this.result - 1);
+  }
+
+  ngOnInit() {}
 }
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  styleUrls: ['./exercice2.component.css'],
 })
 export class Exercice2Component implements OnInit {
+  @ViewChild('myInput', { static: false })
+  public el: ElementRef<HTMLInputElement>;
+
   public valeur: string;
-
-  constructor() { }
-
-  ngOnInit() {
+  inputText() {
+    this.valeur = this.el.nativeElement.value;
   }
+  constructor() {}
 
+  ngOnInit() {}
 }
